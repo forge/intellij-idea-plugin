d611d942ef49e31a1136192ba70dbeb7755080e1
George Gastaldi <gegastaldi@gmail.com>
2013-04-24 18:43:37 -0300

    Added Action

---
+++ src/main/java/org/jboss/forge/plugin/idea/actions/ShowForgeMenuAction.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ * <p/>
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.plugin.idea.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class ShowForgeMenuAction extends AnAction
+{
+   public void actionPerformed(AnActionEvent e)
+   {
+
+      System.out.println("IT WORKS !");
+   }
+}
