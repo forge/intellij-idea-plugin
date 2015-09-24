68f962a9e686463fb965ed41257e83da9c7d66c2
George Gastaldi <gegastaldi@gmail.com>
2015-09-24 14:15:58 -0300

    Commands without inputs should not render an empty dialog

--- src/main/java/org/jboss/forge/plugin/idea/ui/CommandListPopupBuilder.java
+++ src/main/java/org/jboss/forge/plugin/idea/ui/CommandListPopupBuilder.java
@@ -33,7 +33,9 @@ import org.jboss.forge.plugin.idea.runtime.UIProgressMonitorImpl;
 import org.jboss.forge.plugin.idea.runtime.UIRuntimeImpl;
 import org.jboss.forge.plugin.idea.service.ForgeService;
 import org.jboss.forge.plugin.idea.service.PluginService;
+import org.jboss.forge.plugin.idea.ui.wizard.ForgeCommandTask;
 import org.jboss.forge.plugin.idea.ui.wizard.ForgeWizardDialog;
+import org.jboss.forge.plugin.idea.util.ForgeNotifications;
 
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupAdapter;
@@ -51,160 +53,174 @@ import com.intellij.util.Function;
  */
 public class CommandListPopupBuilder
 {
-    private static final Icon FORGE_ICON = new ImageIcon(CommandListPopupBuilder.class.getResource("/icons/forge.png"));
-    private static volatile boolean active;
+   private static final Icon FORGE_ICON = new ImageIcon(CommandListPopupBuilder.class.getResource("/icons/forge.png"));
+   private static volatile boolean active;
 
-    private UIContext uiContext;
-    private List<UICommand> commands;
-    private List<UICommand> recentCommmands;
+   private UIContext uiContext;
+   private List<UICommand> commands;
+   private List<UICommand> recentCommmands;
 
-    public CommandListPopupBuilder setUIContext(UIContext uiContext)
-    {
-        this.uiContext = uiContext;
-        return this;
-    }
+   public CommandListPopupBuilder setUIContext(UIContext uiContext)
+   {
+      this.uiContext = uiContext;
+      return this;
+   }
 
-    public CommandListPopupBuilder setCommands(List<UICommand> commands)
-    {
-        this.commands = commands;
-        return this;
-    }
+   public CommandListPopupBuilder setCommands(List<UICommand> commands)
+   {
+      this.commands = commands;
+      return this;
+   }
 
-    public CommandListPopupBuilder setRecentCommands(List<UICommand> recentCommands)
-    {
-        this.recentCommmands = recentCommands;
-        return this;
-    }
+   public CommandListPopupBuilder setRecentCommands(List<UICommand> recentCommands)
+   {
+      this.recentCommmands = recentCommands;
+      return this;
+   }
 
-    public static boolean isActive()
-    {
-        return active;
-    }
+   public static boolean isActive()
+   {
+      return active;
+   }
 
-    public JBPopup build()
-    {
-        active = true;
+   public JBPopup build()
+   {
+      active = true;
 
-        List<UICommand> allCommands = new ArrayList<>();
-        allCommands.addAll(commands);
-        allCommands.addAll(recentCommmands);
+      List<UICommand> allCommands = new ArrayList<>();
+      allCommands.addAll(commands);
+      allCommands.addAll(recentCommmands);
 
-        Map<UICommand, UICommandMetadata> metadataIndex = indexMetadata(allCommands, uiContext);
-        Map<String, List<UICommand>> categories = categorizeCommands(commands, recentCommmands, metadataIndex);
-        List<Object> elements = categoriesToList(sortCategories(categories, metadataIndex));
-        Map<Object, String> filterIndex = indexFilterData(elements, categories, metadataIndex);
+      Map<UICommand, UICommandMetadata> metadataIndex = indexMetadata(allCommands, uiContext);
+      Map<String, List<UICommand>> categories = categorizeCommands(commands, recentCommmands, metadataIndex);
+      List<Object> elements = categoriesToList(sortCategories(categories, metadataIndex));
+      Map<Object, String> filterIndex = indexFilterData(elements, categories, metadataIndex);
 
-        JBList list = buildJBList(elements, metadataIndex);
-        JBPopup popup = buildPopup(list, filterIndex);
+      JBList list = buildJBList(elements, metadataIndex);
+      JBPopup popup = buildPopup(list, filterIndex);
 
-        return popup;
-    }
+      return popup;
+   }
 
-    private JBList buildJBList(List<Object> elements, final Map<UICommand, UICommandMetadata> metadataIndex)
-    {
-        final JBList list = new JBList();
-        DefaultListModel model = new DefaultListModel();
-        model.setSize(elements.size());
+   private JBList buildJBList(List<Object> elements, final Map<UICommand, UICommandMetadata> metadataIndex)
+   {
+      final JBList list = new JBList();
+      DefaultListModel model = new DefaultListModel();
+      model.setSize(elements.size());
 
-        list.setCellRenderer(new ListCellRendererWrapper<Object>()
-        {
-            @Override
-            public void customize(JList list, Object data, int index,
-                                  boolean selected, boolean hasFocus)
+      list.setCellRenderer(new ListCellRendererWrapper<Object>()
+      {
+         @Override
+         public void customize(JList list, Object data, int index,
+                  boolean selected, boolean hasFocus)
+         {
+            if (data instanceof UICommand)
             {
-                if (data instanceof UICommand)
-                {
-                    setIcon(FORGE_ICON);
+               setIcon(FORGE_ICON);
 
-                    UICommand command = (UICommand) data;
-                    UICommandMetadata metadata = metadataIndex.get(command);
+               UICommand command = (UICommand) data;
+               UICommandMetadata metadata = metadataIndex.get(command);
 
-                    setText(metadata.getName());
-                    setToolTipText(metadata.getDescription());
-                }
-                else if (data instanceof String)
-                {
-                    String string = (String) data;
-
-                    setText(string);
-                    setSeparator();
-                }
+               setText(metadata.getName());
+               setToolTipText(metadata.getDescription());
             }
-        });
-
-        for (int i = 0; i < elements.size(); i++)
-        {
-            model.set(i, elements.get(i));
-        }
-        list.setModel(model);
-
-        return list;
-    }
-
-    private JBPopup buildPopup(final JBList list,
-                               final Map<Object, String> filterIndex)
-    {
-        final PopupChooserBuilder listPopupBuilder = JBPopupFactory.getInstance().createListPopupBuilder(list);
-        listPopupBuilder.setTitle("Run a Forge command");
-        listPopupBuilder.setResizable(true);
-        listPopupBuilder.addListener(new JBPopupAdapter()
-        {
-            @Override
-            public void onClosed(LightweightWindowEvent event)
+            else if (data instanceof String)
             {
-                CommandListPopupBuilder.this.active = false;
+               String string = (String) data;
+
+               setText(string);
+               setSeparator();
             }
-        });
-        listPopupBuilder.setItemChoosenCallback(new Runnable()
-        {
-            @Override
-            public void run()
+         }
+      });
+
+      for (int i = 0; i < elements.size(); i++)
+      {
+         model.set(i, elements.get(i));
+      }
+      list.setModel(model);
+
+      return list;
+   }
+
+   private JBPopup buildPopup(final JBList list,
+            final Map<Object, String> filterIndex)
+   {
+      final PopupChooserBuilder listPopupBuilder = JBPopupFactory.getInstance().createListPopupBuilder(list);
+      listPopupBuilder.setTitle("Run a Forge command");
+      listPopupBuilder.setResizable(true);
+      listPopupBuilder.addListener(new JBPopupAdapter()
+      {
+         @Override
+         public void onClosed(LightweightWindowEvent event)
+         {
+            CommandListPopupBuilder.this.active = false;
+         }
+      });
+      listPopupBuilder.setItemChoosenCallback(new Runnable()
+      {
+         @Override
+         public void run()
+         {
+            Object selectedObject = list.getSelectedValue();
+            if (selectedObject instanceof UICommand)
             {
-                Object selectedObject = list.getSelectedValue();
-                if (selectedObject instanceof UICommand)
-                {
-                    UICommand selectedCommand = (UICommand) selectedObject;
+               UICommand selectedCommand = (UICommand) selectedObject;
 
-                    // Make sure that this cached command is still enabled
-                    if (selectedCommand.isEnabled(uiContext))
-                    {
-                        openWizard(selectedCommand);
-                    }
-                }
+               // Make sure that this cached command is still enabled
+               if (selectedCommand.isEnabled(uiContext))
+               {
+                  openWizard(selectedCommand);
+               }
             }
-        });
-        listPopupBuilder.setFilteringEnabled(new Function<Object, String>()
-        {
-            @Override
-            public String fun(Object object)
-            {
-                return filterIndex.get(object);
-            }
-        });
+         }
+      });
+      listPopupBuilder.setFilteringEnabled(new Function<Object, String>()
+      {
+         @Override
+         public String fun(Object object)
+         {
+            return filterIndex.get(object);
+         }
+      });
 
-        return listPopupBuilder.createPopup();
-    }
+      return listPopupBuilder.createPopup();
+   }
 
-    private void openWizard(UICommand command)
-    {
-        String name = command.getMetadata(uiContext).getName();
-        command = ForgeService.getInstance()
-                .getCommandFactory()
-                .getNewCommandByName(uiContext, name);
+   private void openWizard(UICommand command)
+   {
+      String name = command.getMetadata(uiContext).getName();
+      command = ForgeService.getInstance()
+               .getCommandFactory()
+               .getNewCommandByName(uiContext, name);
 
-        // Since wizard will be able to modify Command state, the cache should be invalidated
-        PluginService.getInstance().invalidateAndReloadCommands(uiContext);
+      // Since wizard will be able to modify Command state, the cache should be invalidated
+      PluginService.getInstance().invalidateAndReloadCommands(uiContext);
 
-        PluginService.getInstance().addRecentCommand(command, uiContext);
+      PluginService.getInstance().addRecentCommand(command, uiContext);
 
-        UIProgressMonitor monitor = new UIProgressMonitorImpl();
+      UIProgressMonitor monitor = new UIProgressMonitorImpl();
 
-        ((UIContextImpl) uiContext).setProgressMonitor(monitor);
-        UIRuntime uiRuntime = new UIRuntimeImpl(monitor);
-        CommandControllerFactory controllerFactory = ForgeService.getInstance().getCommandControllerFactory();
-        CommandController controller = controllerFactory.createController(uiContext, uiRuntime, command);
-
-        ForgeWizardDialog dialog = new ForgeWizardDialog(controller);
-        dialog.show();
-    }
+      ((UIContextImpl) uiContext).setProgressMonitor(monitor);
+      UIRuntime uiRuntime = new UIRuntimeImpl(monitor);
+      CommandControllerFactory controllerFactory = ForgeService.getInstance().getCommandControllerFactory();
+      CommandController controller = controllerFactory.createController(uiContext, uiRuntime, command);
+      try
+      {
+         controller.initialize();
+      }
+      catch (Exception e)
+      {
+         ForgeNotifications.showErrorMessage(e);
+      }
+      if (controller.getInputs().isEmpty() && controller.canExecute())
+      {
+         new ForgeCommandTask(controller).queue();
+      }
+      else
+      {
+         ForgeWizardDialog dialog = new ForgeWizardDialog(controller);
+         dialog.show();
+      }
+   }
 }
