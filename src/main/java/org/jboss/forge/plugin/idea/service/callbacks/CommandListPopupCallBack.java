c0ac4e50ea853e94090f165b27bac5573362c1b5
George Gastaldi <gegastaldi@gmail.com>
2016-05-05 19:09:03 -0300

    Show list when no projects exist

--- src/main/java/org/jboss/forge/plugin/idea/service/callbacks/CommandListPopupCallBack.java
+++ src/main/java/org/jboss/forge/plugin/idea/service/callbacks/CommandListPopupCallBack.java
@@ -15,9 +15,7 @@ import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopup;
-import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.ui.awt.RelativePoint;
 import org.jboss.forge.addon.ui.command.UICommand;
 import org.jboss.forge.addon.ui.context.UIContext;
 import org.jboss.forge.plugin.idea.context.UIContextFactory;
@@ -31,21 +29,21 @@ public class CommandListPopupCallBack implements Runnable
 {
    private final Project project;
    private final Editor editor;
-   private final RelativePoint relativePoint;
+   private final Component component;
    private final VirtualFile[] selectedFiles;
 
    public CommandListPopupCallBack(AnActionEvent event)
    {
       this.project = event.getData(DataKeys.PROJECT);
+      this.editor = event.getData(DataKeys.EDITOR);
+      this.component = event.getData(DataKeys.CONTEXT_COMPONENT);
       VirtualFile[] files = event.getData(DataKeys.VIRTUAL_FILE_ARRAY);
       // If no file is selected, then set project directory as selection
       if (files == null || files.length == 0)
       {
          files = new VirtualFile[] { event.getData(DataKeys.PROJECT_FILE_DIRECTORY) };
       }
-      this.editor = event.getData(DataKeys.EDITOR);
       this.selectedFiles = files;
-      this.relativePoint = JBPopupFactory.getInstance().guessBestPopupLocation(event.getDataContext());
    }
 
    @Override
@@ -65,6 +63,9 @@ public class CommandListPopupCallBack implements Runnable
       {
          list.showCenteredInCurrentWindow(project);
       }
+      else
+      {
+         list.showInCenterOf(component);
+      }
    }
-
 }
