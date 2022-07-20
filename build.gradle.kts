dc1c48df490d50c46ec81ffbd34d008a79e40757
George Gastaldi <gegastaldi@gmail.com>
2022-07-20 16:24:10 -0300

    Do not set untilBuild

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,7 +63,7 @@ intellij {
     version.set(properties("platformVersion"))
     type.set(properties("platformType"))
     downloadSources.set(properties("platformDownloadSources").toBoolean())
-    updateSinceUntilBuild.set(true)
+    updateSinceUntilBuild.set(false)
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
     plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
